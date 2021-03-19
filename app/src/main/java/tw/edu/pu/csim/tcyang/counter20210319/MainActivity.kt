8aac12bec9e5a325fb72a120bc8d6680445f44bb
tcyang <tcyang@gm.pu.edu.tw>
2021-03-19 15:44:50 +0800

    btn的OnClick事件

---
+++ app/src/main/java/tw/edu/pu/csim/tcyang/counter20210319/MainActivity.kt
@@ -0,0 +1,30 @@
+package tw.edu.pu.csim.tcyang.counter20210319
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var counter:Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txv.text = counter.toString()
+    }
+
+
+    fun Add(v:View){
+        if (v.id == R.id.btn){
+            counter++
+        }
+        else{
+            counter += 2
+        }
+        txv.text = counter.toString()
+    }
+
+
+}
